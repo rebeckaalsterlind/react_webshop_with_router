5d4c976f266a3afb28bb5f6122fa42bccad2cda2
Rebecka Alsterlind <[rebecka.alsterlind@gmail.com]>
2021-08-24 12:23:19 +0200

    product links working

--- src/Product.js
+++ src/Product.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 
-const prod = require('./produkter.json');
+const prod = require('./products/produkter.json');
 console.log('data', prod);
 
 function Product(props) {
  
  const result = prod.find( ({ id }) => id == props.match.params.id );
-
- console.log('result', result);
+ let url = require('./products/' + result.image);
+ console.log('url', url.default);
   return (
     <div className="Product">
       <h2>{result.prodName}</h2>
       <p>{result.price}:-</p>
-      <img src="./../products/sko1.jpg" alt="Girl in a jacket" />
+      <img src={url.default} alt="test" />
     </div>
   );
 }
